eada858861f102a3376576d8b5a84bdd27248a27
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-08-24 11:13:38 +1200

    bind the callback to the object's context

--- pass-function.js
+++ pass-function.js
@@ -1,15 +1,25 @@
 const myCar = {
   speed: 0,
+
   operate(speedLimit, callback) {
     callback(speedLimit)
-    console.log(`myCar is driving at ${this.speed}`)
+    console.log(`operate(): myCar is driving at ${this.speed}`)
+  },
+
+  operateBound(speedLimit, callback) {
+    boundCallback = callback.bind(this)
+    boundCallback(speedLimit)
+    console.log(`operateBound(): myCar is driving at ${this.speed}`)
   },
 }
 
 function drive(speedLimit) {
   this.speed = speedLimit
-  console.log(`Start driving at ${this.speed} kph`)
+  console.log(`drive(): Start driving at ${this.speed} kph`)
 }
 
 // drive's `this` references the global scope, so it outputs `undefined`
 myCar.operate(55, drive)
+
+// to overcome this, bind the callback to the `this` context of the object
+myCar.operateBound(65, drive)
